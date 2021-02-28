98eb51b165cd411c0c35b828883f6687dabe5e1a
Julio Eduardo Canizalez Salinas <juliocanizalez@outlook.com>
2021-02-27 21:45:55 -0600

    :hammer: now a random number is generated when you click any dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,57 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dice App'),
+          backgroundColor: Colors.red,
         ),
+        body: DicePage(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  final _random = new Random();
+  int min = 1;
+  int max = 7;
+
+  int dice1 = 1, dice2 = 3;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      dice1 = min + _random.nextInt(max - min);
+                    });
+                  },
+                  child: Image.asset('assets/img/dice$dice1.png'))),
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    dice2 = min + _random.nextInt(max - min);
+                  });
+                },
+                child: Image.asset('assets/img/dice$dice2.png')),
+          ),
+        ],
+      ),
     );
   }
 }
